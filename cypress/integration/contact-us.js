39c493bcef5e252c18d68b7c581d7f7e7ea6df11
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-10 02:59:36 -0600

    Update positive and negative tests

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -9,9 +9,16 @@ describe("Test Contact Us form via WebdriverUni", () => {
         cy.get('[name="email"]').type("testemail@testemail.com")
         cy.get('textarea.feedback-input').type("Add some comments")
         cy.get('[type="submit"]').click()
+        cy.contains("Thank You for your Message!")
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are mandatory", () => {
         //cypress code
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Ivan")
+        cy.get('[name="email"]').type("testemail@testemail.com")
+        cy.get('textarea.feedback-input').type("Add some comments")
+        cy.get('[type="submit"]').click()
+        cy.contains("Error: all fields are required")
     })
 })
