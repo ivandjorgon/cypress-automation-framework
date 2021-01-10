a0fd4749a2cf2b2c06b7a838f9ef4bde5801afee
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-10 02:14:20 -0600

    Update contact-us

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -1,9 +1,13 @@
+/// <reference types="Cypress" />
+
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-        //cypress code goes here
+        //cypress code
+        cy.visit("http://www.webdriveruniversity.com/")
+        cy.get('#contact-us > .thumbnail').click()
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are mandatory", () => {
-        //cypress code goes here
+        //cypress code
     })
 })
