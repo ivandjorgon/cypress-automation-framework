528d7ea3d1995d7b37a8529a7a1b806fd4250f50
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-10 14:21:06 -0600

    Add dynamic contact-us selectors

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -3,11 +3,11 @@
 describe("Test Contact Us form via Automation Test Store", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.automationteststore.com/");
-        cy.get('a[href="https://automationteststore.com/index.php?rt=content/contact"]').click();
+        cy.get("a[href$='contact']").click();
         cy.get('#ContactUsFrm_first_name').type("Joe");
         cy.get('#ContactUsFrm_email').type("joeemail@joeemail.com");
         cy.get('#ContactUsFrm_enquiry').type("Add some comments here");
-        cy.get('.col-md-6 > .btn').click()
+        cy.get("button[title = 'Submit']").click()
         cy.contains('Your enquiry has been successfully sent to the store owner!');
     })
 })
