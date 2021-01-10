9e4687c17dd64498707b4272b168bd0d57fd6d7b
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-10 13:37:31 -0600

    Update contact-us file

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -2,13 +2,12 @@
 
 describe("Test Contact Us form via Automation Test Store", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-        cy.visit("https://www.automationteststore.com/")
-
-        // cy.get('[name="first_name"]').type("Ivan")
-        // cy.get('[name="last_name"]').type("Djorgon")
-        // cy.get('[name="email"]').type("testemail@testemail.com")
-        // cy.get('textarea.feedback-input').type("Add some comments")
-        // cy.get('[type="submit"]').click()
-        // cy.contains("Thank You for your Message!")
+        cy.visit("https://www.automationteststore.com/");
+        cy.get('.info_links_footer > :nth-child(5) > a').click();
+        cy.get('#ContactUsFrm_first_name').type("Joe");
+        cy.get('#ContactUsFrm_email').type("joeemail@joeemail.com");
+        cy.get('#ContactUsFrm_enquiry').type("Add some comments here");
+        cy.get('.col-md-6 > .btn').click()
+        cy.contains('Your enquiry has been successfully sent to the store owner!');
     })
 })
