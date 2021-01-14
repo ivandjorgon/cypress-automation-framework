4f6a68558f6947716bd492f7c2284dd2abaa8774
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-13 22:43:35 -0600

    Add variables and cy commands file

---
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe("Verifying variables, cypress commands and jquery commands", () => {
+    it("Navigated to specific product pages", () => {
+        cy.visit("http://automationteststore.com/");
+        // The following will fail - not recommended
+        // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup");
+        // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare");
+        // makeupLink.click();
+        // skincareLink.click();
+
+        // The following will pass - not recommended either
+        // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup");
+        // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare");
+        // makeupLink.click();
+        // skincareLink.click();
+
+        // Recommended Approach
+        cy.get("a[href*='product/category&path=']").contains("Makeup").click()
+        cy.get("a[href*='product/category&path=']").contains("Skincare").click()
+    });
+})
