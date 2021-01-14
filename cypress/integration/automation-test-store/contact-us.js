4bddffa5045b354136dad82d63159275c0162bcf
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-13 22:21:28 -0600

    Add inspect-item file and update contact-us file

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -3,7 +3,9 @@
 describe("Test Contact Us form via Automation Test Store", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.automationteststore.com/");
-        cy.get("a[href$='contact']").click();
+        cy.get("a[href$='contact']").click().then(function(itemText) {
+            console.log("Clicked on the following item: " + itemText.text())
+        });
         // cy.xpath("//a[contains(@href, 'contact')]").click()
         cy.get('#ContactUsFrm_first_name').type("Joe");
         cy.get('#ContactUsFrm_email').type("joeemail@joeemail.com");
@@ -12,5 +14,6 @@ describe("Test Contact Us form via Automation Test Store", () => {
         cy.get("button[title = 'Submit']").click();
         cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!');
         cy.contains('Your enquiry has been successfully sent to the store owner!');
+        cy.log("Test has completed!");
     })
 })
