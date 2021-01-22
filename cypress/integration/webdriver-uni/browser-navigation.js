cfb859244a7fc502d4ee4267a7685a0ed514f41e
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-21 23:39:07 -0600

    Add browser nav file

---
+++ cypress/integration/webdriver-uni/browser-navigation.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("Validate WebdriverUni homepage links", () => {
+    it("Confirm links redirect to the correct pages", () => {
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true})
+        cy.url().should('include', 'contactus')
+
+        cy.go('back')
+    })
+})
