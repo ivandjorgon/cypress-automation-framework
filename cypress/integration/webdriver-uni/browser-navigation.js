d3ce09f263bb6a2cde8ab0b0b3a1149c911e3d17
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-21 23:50:35 -0600

    Add assertions

--- cypress/integration/webdriver-uni/browser-navigation.js
+++ cypress/integration/webdriver-uni/browser-navigation.js
@@ -9,9 +9,15 @@ describe("Validate WebdriverUni homepage links", () => {
 
         cy.go('back')
         cy.reload()
+        cy.url().should('include', 'http://www.webdriveruniversity.com')
         //cy.reload(true) // reload without using cache
 
         cy.go('forward')
         cy.url().should('include', 'contactus')
+
+        cy.go('back')
+        cy.get('#login-portal').invoke('removeAttr', 'target').click({force:true})
+        cy.url().should('include', 'Login-Portal')
+        cy.go('back')
     })
 })
