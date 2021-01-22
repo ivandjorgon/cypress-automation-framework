d9f5d3e29d80ce97113f14528680621240efc0fd
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-21 23:22:53 -0600

    Update same origin tests

--- cypress/integration/other/same-origin-policy.js
+++ cypress/integration/other/same-origin-policy.js
@@ -1,12 +1,13 @@
 /// <reference types="Cypress" />
 
 describe("Cypress web security", () => {
-    it.only("Validate visiting two different domains", () => {
-        cy.visit('http://www.webdriveruniversity.com/')
+    it("Validate visiting two different domains", () => {
+        cy.visit('http://www.webdriveruniversity.com/');
         cy.visit('https://www.automationteststore.com/');
     })
 
-    it("Should not be able to submit a successful submission via contact us form as all fields are mandatory", () => {
-
+    it("Validate visiting two different domains via user actions", () => {
+        cy.visit('http://www.webdriveruniversity.com/');
+        cy.get('#automation-test-store').invoke('removeAttr', 'target').click()
     })
 })
