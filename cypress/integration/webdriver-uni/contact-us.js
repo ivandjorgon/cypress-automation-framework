5edd59431c4fb23315c9f922b33501a22cd21dc8
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-21 22:54:52 -0600

    Add invoke remoteAttr to handle multiple browser tabs

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -2,7 +2,9 @@
 
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true})
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
@@ -18,7 +20,9 @@ describe("Test Contact Us form via WebdriverUni", () => {
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are mandatory", () => {
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true})
         cy.get('[name="first_name"]').type("Ivan")
         cy.get('[name="email"]').type("testemail@testemail.com")
         cy.get('textarea.feedback-input').type("Add some comments")
