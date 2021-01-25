f9fa2b67ce7d89d2e5109be2ffd0fc60b6c04a3a
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-24 18:12:46 -0600

    Update checkbox assertion

--- cypress/integration/webdriver-uni/radio-buttons.js
+++ cypress/integration/webdriver-uni/radio-buttons.js
@@ -19,5 +19,7 @@ describe("Verify radio buttons via webdriveruni", () => {
         cy.get("[value='lettuce']").should('be.checked')
 
         cy.get("[value='pumpkin']").should('not.be.checked')
+
+        cy.get("[value='cabbage']").should('be.disabled')
     })
 })
