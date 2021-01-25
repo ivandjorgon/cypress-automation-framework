192e6929f13dfe2ba02b3d2e5582aebcb5ee65a7
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-25 16:49:22 -0600

    Update autocomplete test

--- cypress/integration/webdriver-uni/autocomplete-dropdown-list.js
+++ cypress/integration/webdriver-uni/autocomplete-dropdown-list.js
@@ -16,6 +16,19 @@ describe("Verify autocomplete dropdown lists", () => {
                 cy.get('#submit-button').click();
                 cy.url().should('include', productToSelect);
             }
+        }).then(() => {
+            cy.get('#myInput').type('g')
+            cy.get('#myInputautocomplete-list > *').each(($el, index, $list) => {
+                const prod = $el.text();
+                const productToSelect = 'Grapes';
+    
+                if(prod === productToSelect) {
+                    $el.click();
+    
+                    cy.get('#submit-button').click();
+                    cy.url().should('include', productToSelect);
+                }
+            })
         })
     })
 })
