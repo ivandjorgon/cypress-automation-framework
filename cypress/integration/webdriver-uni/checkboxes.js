9ae95090ceeebe7a472e94c7f6be94f826999723
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-26 16:51:25 -0600

    Add beforeEach hook

--- cypress/integration/webdriver-uni/checkboxes.js
+++ cypress/integration/webdriver-uni/checkboxes.js
@@ -1,10 +1,13 @@
 /// <reference types="Cypress" />
 
 describe("Verify checkboxes via webdriveruni", () => {
-    it("Check and validate option 1 checkbox", () => {
+
+    beforeEach(function() {
         cy.visit("http://www.webdriveruniversity.com")
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+    })
 
+    it("Check and validate option 1 checkbox", () => {
         // Option 1 check
         cy.get('#checkboxes > :nth-child(1) > input').as('option-1')
         cy.get('@option-1').check().should('be.checked')
@@ -16,18 +19,12 @@ describe("Verify checkboxes via webdriveruni", () => {
     })
 
     it("Uncheck and validate option 3 checkbox", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
-
         // Option 3 uncheck
         cy.get(':nth-child(5) > input').as('option-3')
         cy.get('@option-3').uncheck().should('not.be.checked')
     })
 
     it("Check and validate multiple checkboxes", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
-
         cy.get("input[type='checkbox']").check(["option-1", "option-2", "option-3", "option-4"]).should('be.checked')
         cy.get("input[type='checkbox']").should('be.enabled')
     })
