3a0422bb8f1996f3d839628749a41c089c7d8d4b
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-26 00:05:20 -0600

    Update json-example.js

--- cypress/integration/other/json-example.js
+++ cypress/integration/other/json-example.js
@@ -4,6 +4,7 @@ describe("JSON Object", () => {
     it("Json Object Examples", () => {
         const exampleObject = {"key": "Tim", "key2": "Jones"}
         const exampleArrayOfValues = ["Sophie", "Rose", "Howard"]; 
+        const exampleArrayOfObjects = [{"key": "Luke"}, {"key2": "Peter"}, {"key3": "Tom"}];
 
         const users = {
             "firstName": "Joe",
@@ -36,5 +37,9 @@ describe("JSON Object", () => {
         cy.log(users.lastName);
         cy.log(users.Students[0].lastName);
         cy.log(users.Students[1].lastName);
+
+        cy.log(exampleArrayOfObjects[0]["key"]);
+        cy.log(exampleArrayOfObjects[1]["key2"]);
+        cy.log(exampleArrayOfObjects[2]["key3"]);
     })
 })
