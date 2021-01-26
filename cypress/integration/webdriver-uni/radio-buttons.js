79c170665b11cfd3303831be49ffb512368bcb9d
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-26 16:45:58 -0600

    Add before hook

--- cypress/integration/webdriver-uni/radio-buttons.js
+++ cypress/integration/webdriver-uni/radio-buttons.js
@@ -1,25 +1,23 @@
 /// <reference types="Cypress" />
 
 describe("Verify radio buttons via webdriveruni", () => {
-    it("Check and validate radio button", () => {
+
+    before(function() {
         cy.visit("http://www.webdriveruniversity.com")
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+    })
+
+    it("Check and validate radio button", () => {
         cy.get('#radio-buttons').find("[type='radio']").first().check()
         cy.get('#radio-buttons').find("[type='radio']").eq(1).check()
     })
 
     it("Validate the states of specific radio buttons", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
-
         cy.get("[value='lettuce']").should('not.be.checked')
         cy.get("[value='pumpkin']").should('be.checked')
-
         cy.get("[value='lettuce']").check()
         cy.get("[value='lettuce']").should('be.checked')
-
         cy.get("[value='pumpkin']").should('not.be.checked')
-
         cy.get("[value='cabbage']").should('be.disabled')
     })
 })
