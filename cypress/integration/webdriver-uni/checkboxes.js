916186957df860e580e91b6d7782284aba9af9f8
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-28 14:36:51 -0600

    Update beforeEach function

--- cypress/integration/webdriver-uni/checkboxes.js
+++ cypress/integration/webdriver-uni/checkboxes.js
@@ -3,7 +3,7 @@
 describe("Verify checkboxes via webdriveruni", () => {
 
     beforeEach(function() {
-        cy.visit("http://www.webdriveruniversity.com")
+        cy.visit("/")
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
     })
 
