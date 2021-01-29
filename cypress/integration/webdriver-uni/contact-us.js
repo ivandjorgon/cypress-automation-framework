0904107ed48cc35119e1ea91e092790b3a389a6a
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-29 14:25:05 -0600

    Refactor code

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -3,6 +3,8 @@ import Contact_Us_PO from '../../support/pageObjects/webdriver-uni/Contact_Us_PO
 /// <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
+    const homepage_PO = new HomePage_PO();
+    const contact_Us_PO = new Contact_Us_PO();
 
     before(function() {
         cy.fixture('example').then(function(data) {
@@ -12,45 +14,18 @@ describe("Test Contact Us form via WebdriverUni", () => {
     })
 
     beforeEach(function() {
-        //cy.visit("http://www.webdriveruniversity.com")
-        //cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true})
-        const homepage_PO = new HomePage_PO();
         homepage_PO.visitHomepage();
         homepage_PO.clickOn_ContactUs_Button();
-        //const contact_PO = new Contact_Us_PO();
     })
 
     it("Should be able to submit a successful submission via contact us form", () => {
-        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
-        //cy.visit("http://www.webdriveruniversity.com")
-        //cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true})
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
-        //cy.get('#contact-us > .thumbnail').click({force: true})
-        // cy.get('[name="first_name"]').type(data.first_name)
-        // cy.get('[name="last_name"]').type(data.last_name)
-        // cy.get('[name="email"]').type(data.email)
-        // cy.get('textarea.feedback-input').type("Add some comments")
-        // cy.get('[type="submit"]').click()
-        // cy.get('#contact_reply > h1').should('have.text', 'Thank You for your Message!')
-        // or cy.get('h1').should('have.text', 'Thank You for your Message!')
-        // cy.contains("Thank You for your Message!")
-        //cy.webdriverUni_ContactForm_Submission(Cypress.env("first_name"), data.last_name, data.email, "Add test comment", 'h1', 'Thank You for your Message!');
-        const contact_Us_PO = new Contact_Us_PO();
         contact_Us_PO.contactForm_Submission(Cypress.env("first_name"), data.last_name, data.email, "Add test comment", "h1", "Thank You for your Message!");
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are mandatory", () => {
-        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
-        //cy.visit("http://www.webdriveruniversity.com")
-        //cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true})
-        // cy.get('[name="first_name"]').type(data.first_name)
-        // cy.get('[name="email"]').type(data.email)
-        // cy.get('textarea.feedback-input').type("Add some comments")
-        // cy.get('[type="submit"]').click()
-        // cy.get('body').contains('Error: all fields are required')  
-        // cy.contains("Error: all fields are required") 
-        cy.webdriverUni_ContactForm_Submission(data.first_name, data.last_name, " ", "Add test comment", 'body', 'Error: Invalid email address');
+        contact_Us_PO.contactForm_Submission(Cypress.env("first_name"), data.last_name, " ", "Add test comment", "body", "Error: Invalid email address");
     })
 })
