390c523ed9a11fed9f88200b4c78c7e8fa156eb0
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-01-31 21:19:04 -0600

    Update data-tables.js

--- cypress/integration/webdriver-uni/data-tables.js
+++ cypress/integration/webdriver-uni/data-tables.js
@@ -13,9 +13,20 @@ describe("Handling data via webdriveruni", () => {
     })
 
     it("Calculate and assert the total age of all users", () => {
+        var userDetails = [];
+        let number = 0;
         cy.get('#thumbnail-1 td').each(($el, index, $list) => {
-            
+            userDetails[index] = $el.text();
+        }).then(() => {
+            var i;
+            for(i = 0; i < userDetails.length; i++) {
+                if(Number(userDetails[i])) {
+                    number += Number(userDetails[i]);
+                }
+                //cy.log(userDetails[i]);
+            }
+            cy.log("Found total age: " + number);
+            expect(number).to.eq(322);
         })
     })
-
 })
