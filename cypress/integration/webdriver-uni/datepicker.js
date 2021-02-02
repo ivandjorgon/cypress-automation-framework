631332c2b63931539cd26df1d06a490de40f15b1
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-02-02 16:23:13 -0600

    Add select month and year function

--- cypress/integration/webdriver-uni/datepicker.js
+++ cypress/integration/webdriver-uni/datepicker.js
@@ -5,12 +5,31 @@ describe("Test Datepicker via webdriveruni", () => {
         cy.visit("http://www.webdriveruniversity.com")
         cy.get('#datepicker').invoke('removeAttr', 'target').click({force:true})
 
-        let date = new Date();
-        date.setDate(date.getDate());
-        cy.log(date.getDate()) // get current day
+        // let date = new Date();
+        // date.setDate(date.getDate());
+        // cy.log(date.getDate()) // get current day
 
-        let date2 = new Date();
-        date2.setDate(date.getDate() + 5)
-        cy.log(date2.getDate()) // get current day + 5
+        // let date2 = new Date();
+        // date2.setDate(date.getDate() + 5)
+        // cy.log(date2.getDate()) // get current day + 5
+
+        var date = new Date();
+        date.setDate(date.getDate() + 5);
+        
+        var futureYear = date.getFullYear();
+        var futureMonth = date.toLocaleString("default", {month: "long"});
+        var futureDay = date.getDate();
+
+        cy.log("Future year to select: " + futureYear);
+        cy.log("Future month to select: " + futureMonth);
+        cy.log("Future day to select: " + futureDay);
+
+        function selectMonthandYear() {
+            cy.get('.datepicker-dropdown').find('.datepicker-switch').first().then(currentDate => {
+                if(!currentDate.text().includes(futureYear)) {
+                    cy.get('.next').first().click();
+                }
+            })
+        }
     })
 })
