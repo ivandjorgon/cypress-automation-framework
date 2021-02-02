073384367dc3a985603023309924efaee233c955
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-02-02 12:59:14 -0600

    Add test to select date from datepicker

--- cypress/integration/webdriver-uni/datepicker.js
+++ cypress/integration/webdriver-uni/datepicker.js
@@ -4,5 +4,13 @@ describe("Test Datepicker via webdriveruni", () => {
     it("Select date from the datepicker", () => {
         cy.visit("http://www.webdriveruniversity.com")
         cy.get('#datepicker').invoke('removeAttr', 'target').click({force:true})
+
+        let date = new Date();
+        date.setDate(date.getDate());
+        cy.log(date.getDate()) // get current day
+
+        let date = new Date();
+        date.setDate(date.getDate() + 5)
+        cy.log(date.getDate()) // get current day + 5
     })
 })
