5edc18a7dd276203274cc66db92938d812933da0
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-02-02 19:51:44 -0600

    Add csv file upload test

--- cypress/integration/webdriver-uni/file-upload.js
+++ cypress/integration/webdriver-uni/file-upload.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe("Test File Upload via webdriveruni", () => {
-    it("Upload a file....", () => {
+    it("Upload a png file....", () => {
         cy.visit("http://www.webdriveruniversity.com")
         cy.get('#file-upload').invoke('removeAttr', 'target').click({force:true})
 
@@ -11,6 +11,25 @@ describe("Test File Upload via webdriveruni", () => {
                     fileContent,
                     fileName: "idjorgon.png",
                     mimeType: "image/png"
+                },
+                {
+                    uploadType: "input"
+                }
+            )
+        })
+        cy.get("#submit-button").click();
+    })
+
+    it("Upload a csv file....", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#file-upload').invoke('removeAttr', 'target').click({force:true})
+
+        cy.fixture("file_csv_test.csv", "base64").then(fileContent => {
+            cy.get("#myFile").attachFile(
+                {
+                    fileContent,
+                    fileName: "file_csv_test.csv",
+                    mimeType: "text/csv"
                 },
                 {
                     uploadType: "input"
