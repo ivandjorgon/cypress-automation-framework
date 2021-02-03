98fd81996596cc01a9cd8498e5a922ec18f83f50
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-02-02 20:48:26 -0600

    Refactor file upload

--- cypress/integration/webdriver-uni/file-upload.js
+++ cypress/integration/webdriver-uni/file-upload.js
@@ -1,10 +1,15 @@
+import HomePage_PO from '../../support/pageObjects/webdriver-uni/Homepage_PO'
 /// <reference types="Cypress" />
 
 describe("Test File Upload via webdriveruni", () => {
-    it("Upload a png file....", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#file-upload').invoke('removeAttr', 'target').click({force:true})
+    const homepage_PO = new HomePage_PO();
+    
+    beforeEach(function() {
+        homepage_PO.visitHomepage();
+        homepage_PO.clickOn_FileUpload_Button();
+    })
 
+    it("Upload a png file....", () => {
         cy.fixture("idjorgon.png", "base64").then(fileContent => {
             cy.get("#myFile").attachFile(
                 {
@@ -21,9 +26,6 @@ describe("Test File Upload via webdriveruni", () => {
     })
 
     it("Upload a csv file....", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#file-upload').invoke('removeAttr', 'target').click({force:true})
-
         cy.fixture("file_csv_test.csv", "base64").then(fileContent => {
             cy.get("#myFile").attachFile(
                 {
@@ -40,9 +42,6 @@ describe("Test File Upload via webdriveruni", () => {
     })
 
     it("Upload a jpg file....", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#file-upload').invoke('removeAttr', 'target').click({force:true})
-
         cy.fixture("gallery8.jpg", "base64").then(fileContent => {
             cy.get("#myFile").attachFile(
                 {
@@ -59,8 +58,6 @@ describe("Test File Upload via webdriveruni", () => {
     })
 
     it("Upload no file....", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#file-upload').invoke('removeAttr', 'target').click({force:true})
         cy.get("#submit-button").click();
     })
 })
