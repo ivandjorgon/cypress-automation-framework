5604b697e59fbbd38e259dec818384e530157a28
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-02-02 18:16:25 -0600

    Add import cypress-file-upload

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -55,3 +55,4 @@ Cypress.Commands.add("webdriverUni_ContactForm_Submission", (firstName, lastName
 
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import 'cypress-file-upload';
