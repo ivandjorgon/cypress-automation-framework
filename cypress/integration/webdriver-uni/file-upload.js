00972e8e50370915ce33410bf4fd1e073dc0e296
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-02-02 19:27:52 -0600

    Add test for file upload

--- cypress/integration/webdriver-uni/file-upload.js
+++ cypress/integration/webdriver-uni/file-upload.js
@@ -5,5 +5,18 @@ describe("Test File Upload via webdriveruni", () => {
         cy.visit("http://www.webdriveruniversity.com")
         cy.get('#file-upload').invoke('removeAttr', 'target').click({force:true})
 
+        cy.fixture("idjorgon.png", "base64").then(fileContent => {
+            cy.get("#myFile").attachFile(
+                {
+                    fileContent,
+                    fileName: "idjorgon.png",
+                    mimeType: "image/png"
+                },
+                {
+                    uploadType: "input"
+                }
+            )
+        })
+        cy.get("#submit-button").click();
     })
 })
