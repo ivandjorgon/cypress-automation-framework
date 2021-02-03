6080e69e023dc8affa676e41dfe33bb917d78597
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-02-02 21:04:47 -0600

    Add file page object submit function and update tests

--- cypress/integration/webdriver-uni/file-upload.js
+++ cypress/integration/webdriver-uni/file-upload.js
@@ -1,8 +1,10 @@
 import HomePage_PO from '../../support/pageObjects/webdriver-uni/Homepage_PO'
+import FileUpload_PO from '../..//support/pageObjects/webdriver-uni/Fileupload_PO'
 /// <reference types="Cypress" />
 
 describe("Test File Upload via webdriveruni", () => {
     const homepage_PO = new HomePage_PO();
+    const fileupload_PO = new FileUpload_PO();
     
     beforeEach(function() {
         homepage_PO.visitHomepage();
@@ -22,7 +24,7 @@ describe("Test File Upload via webdriveruni", () => {
                 }
             )
         })
-        cy.get("#submit-button").click();
+        fileupload_PO.submitFile();
     })
 
     it("Upload a csv file....", () => {
@@ -38,7 +40,7 @@ describe("Test File Upload via webdriveruni", () => {
                 }
             )
         })
-        cy.get("#submit-button").click();
+        fileupload_PO.submitFile();
     })
 
     it("Upload a jpg file....", () => {
@@ -54,10 +56,10 @@ describe("Test File Upload via webdriveruni", () => {
                 }
             )
         })
-        cy.get("#submit-button").click();
+        fileupload_PO.submitFile();
     })
 
     it("Upload no file....", () => {
-        cy.get("#submit-button").click();
+        fileupload_PO.submitFile();
     })
 })
---
+++ cypress/support/pageObjects/webdriver-uni/Fileupload_PO.js
@@ -0,0 +1,6 @@
+class FileUpload_PO {
+    submitFile() {
+        cy.get("#submit-button").click();
+    }
+}
+export default FileUpload_PO;
