b5b6d8d6a909c71611fbb259c7adbbf69b513cde
Ivan Djorgon <ivan_djorgon@hotmail.com>
2021-02-07 00:07:18 -0600

    Configure test for specific browser

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -11,24 +11,28 @@ describe("Test Contact Us form via WebdriverUni", () => {
         cy.fixture('example').then(function(data) {
             //this.data = data;
             globalThis.data = data;
-        })
-    })
+        });
+    });
 
     beforeEach(function() {
         homepage_PO.visitHomepage();
         cy.wait(100);
         homepage_PO.clickOn_ContactUs_Button();
         //cy.pause();
-    })
+    });
 
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
         contact_Us_PO.contactForm_Submission(Cypress.env("first_name"), data.last_name, data.email, "Add test comment", "h1", "Thank You for your Message!");
-    })
+    });
 
     it("Should not be able to submit a successful submission via contact us form as all fields are mandatory", () => {
-        contact_Us_PO.contactForm_Submission(Cypress.env("first_name"), data.last_name, " ", "Add test comment", "body", "Error: Invalid email address");
-    })
-})
+        if(Cypress.isBrowser("firefox")) {
+
+        } else {
+            contact_Us_PO.contactForm_Submission(Cypress.env("first_name"), data.last_name, " ", "Add test comment", "body", "Error: Invalid email address");
+        }
+    });
+});
